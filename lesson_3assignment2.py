09193b8e5905ce734fe065bb385856e22217fad3
Jasmine Gomes <jasminegomesusa@gmail.com>
2021-09-11 11:11:22 -0700

    submitting lesson 3 activity 2

---
+++ lesson_3assignment2.py
@@ -0,0 +1,10 @@
+################################################
+# Create a program to accept words from a user, and add them to a dictionary. 
+# At the end, use print(mydict) to print out the user's work to them
+x = [1,2,3,4,5,6,7]
+length = 0
+for y in x:
+    length += 1
+
+print(length)
+
